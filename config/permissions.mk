100d70cc356e19f9a4e24f5b98a4c772469e6d09
Cesar Matias <cesar.maximo@gmail.com>
2017-02-16 22:32:56 +0000

    permissions: Remove audio.low_latency this is an work-around for Unity game engines
    Test scenario:
    Antutu 3D tests (since it uses an old Unity build)
    Clash of clans (choppy sound)
    Star Wars -Force Arena (choppy sound)
    etc

--- config/permissions.mk
+++ config/permissions.mk
@@ -19,7 +19,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
