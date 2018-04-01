a6ae2be1a4a3a1f5a01f6593c71e5e633c511f72
SamarV-121 <samarvispute121@gmail.com>
2018-04-01 20:53:30 +0530

    libtmkshims: Update xlog symbols
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- libmtkshims/Android.mk
+++ libmtkshims/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_INCLUDE_XLOG_SYMBOLS),true)
     include $(CLEAR_VARS)
 
-    LOCAL_SRC_FILES := xlog.c
+    LOCAL_SRC_FILES := xlog.cpp
     LOCAL_SHARED_LIBRARIES := liblog
     LOCAL_MODULE := libmtkshim_log
     LOCAL_MODULE_TAGS := optional
