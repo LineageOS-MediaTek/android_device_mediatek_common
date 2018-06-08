ce3821cc9d9c19120bf4756ecd174e08f38c43c8
SamarV-121 <samarvispute121@gmail.com>
2018-06-08 02:09:15 -0400

    mt6737: Add common props
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- mt6737.mk
+++ mt6737.mk
@@ -195,6 +195,9 @@ USE_CAMERA_STUB := true
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
+# Common Properties
+TARGET_SYSTEM_PROP := $(COMMON_PATH)/system.prop
+
 # Cyanogenmod H/W Hooks
 BOARD_HARDWARE_CLASS := $(COMMON_PATH)/cmhw
 
