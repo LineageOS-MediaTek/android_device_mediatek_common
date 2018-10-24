c3d92c4917d09a8de37068ae8b0ce21041e9c9ed
SamarV-121 <samarvispute121@gmail.com>
2018-10-24 14:18:43 +0000

    Build wificond and wifilogd
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- common.mk
+++ common.mk
@@ -79,7 +79,9 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    wificond \
+    wifilogd 
 
 #### Copy files ####
 
